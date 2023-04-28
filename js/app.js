32fcd7a6ce58256aecac28cba311620909dad755
Yahor Yankavets <mr.yankovec@gmail.com>
2023-04-28 18:26:23 +0300

    Added js for modal windows

--- js/app.js
+++ js/app.js
@@ -1,23 +1,78 @@
 $(function () {
-  let filter = $('[data-filter]');
+	// filter______________________________
 
-  filter.on('click', function (event) {
-    event.preventDefault();
+	let filter = $('[data-filter]');
 
-    let cat = $(this).data('filter');
+	filter.on('click', function (event) {
+		event.preventDefault();
 
-    if (cat == 'all') {
-      $('[data-cat]').removeClass('hide');
-    } else {
-      $('[data-cat]').each(function () {
-        let workCat = $(this).data('cat');
+		let cat = $(this).data('filter');
 
-        if (workCat !== cat) {
-          $(this).addClass('hide');
-        } else {
-          $(this).removeClass('hide');
-        }
-      });
-    }
-  });
+		if (cat == 'all') {
+			$('[data-cat]').removeClass('hide');
+		} else {
+			$('[data-cat]').each(function () {
+				let workCat = $(this).data('cat');
+
+				if (workCat !== cat) {
+					$(this).addClass('hide');
+				} else {
+					$(this).removeClass('hide');
+				}
+			});
+		}
+	});
+
+	// modal__________________________________
+	const modalCall = $('[data-modal]');
+	const modalClose = $('[data-close]');
+
+	modalCall.on('click', function (event) {
+		event.preventDefault();
+
+		let $this = $(this);
+		let modalId = $this.data('modal');
+
+		$(modalId).addClass('show');
+		$('body').addClass('no-scroll');
+
+		setTimeout(function () {
+			$(modalId).find('.modal__dialog').css({
+				transform: 'scale(1)',
+			}),
+				200;
+		});
+	});
+
+	modalClose.on('click', function (event) {
+		event.preventDefault();
+
+		let $this = $(this);
+		let modalParent = $this.parents('.modal');
+
+		$(modalParent).find('.modal__dialog').css({
+			transform: 'scale(0)',
+		});
+
+		setTimeout(function () {
+			modalParent.removeClass('show');
+			$('body').removeClass('no-scroll');
+		}, 200);
+	});
+
+	$('.modal').on('click', function (event) {
+		let $this = $(this);
+		$this.find('.modal__dialog').css({
+			transform: 'scale(0)',
+		});
+
+		setTimeout(function () {
+			$this.removeClass('show');
+			$('body').removeClass('no-scroll');
+		}, 200);
+	});
+
+	$('.modal__dialog').on('click', function (event) {
+		event.stopPropagation();
+	});
 });
